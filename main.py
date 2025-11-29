9be46a52d7a4891ab1724b4a6e52bb0f9d759283
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-11-29 23:27:39 +0530

    removed Email

--- main.py
+++ main.py
@@ -2,26 +2,13 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import EmailStr,BaseModel
-import httpx
 import os
 load_dotenv()
 
-# class ContactForm(BaseModel):
-#     name : str
-#     email : EmailStr
-#     subject : str
-#     message : str
 
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 URL = f"mongodb+srv://{USERNAME}:{PASSWORD}@portfoliocluster.pcgilwr.mongodb.net/?appName=PortfolioCluster"
-
-# MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
-# MAILGUN_DOMAIN = os.getenv("MAILGUN_DOMAIN")
-
-# if MAILGUN_API_KEY is None:
-#     raise RuntimeError("MAILGUN_API_KEY is not set in environment")
 
 try:
     client = AsyncIOMotorClient(URL)
@@ -54,28 +41,9 @@ def home():
 
 @app.get("/languages")
 async def Languages():
-    data = await lang.find().to_list(100)
+    data = await lang.find().to_list()
     for doc in data:
         doc["_id"] = str(doc["_id"])
     # print(data)
     return data
 
-
-
-# @app.post("/sendmail")
-# async def send(form: ContactForm):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.post(
-#             f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-#             auth=("api", MAILGUN_API_KEY or ""),
-#             data={
-#                 "from": form.email,
-#                 "to": "Alvin <alvingeorge_@outlook.com>",
-#                 "subject": form.subject,
-#                 "text": f"📩 You got a new message!\n\nName : {form.name}\n\nFrom: {form.email}\n\nMessage:\n{form.message}"
-#             }
-#         )
-#     return {"status": response.status_code, "details": response.text}
-
-
-# handler = app
